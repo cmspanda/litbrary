8475a8ca8e4259e010848e191d5a2f7d62101498
sachioross <sachioross@gmail.com>
2023-06-29 23:59:54 +0900

    Configruing for deployment to cloudflare

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,12 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 // rollup.config.js
 export default {
 	input: 'main.js',
-	output: {
-		file: 'bundle.js',
+	output: [{
+		file: 'dist/bundle.js',
 		format: 'cjs'
-	},
+	},{
+		file: 'site/scripts/bundle.js',
+		format: 'cjs'
+	}],
     plugins: [nodeResolve()]
 };
